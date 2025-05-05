932add76dcc1176fedb350df9705e7bc1146d921
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-05 20:02:57 +0530

    python

--- practice.py
+++ practice.py
@@ -911,16 +911,19 @@ print(calendar.month(2003,6))'''
 #         else:
 #             res1+=" "+" "
 #     print(res1)
+      
             
-row=5 
-for i in range(1,row+1):
-    res2=""
-    for j in range(1,row+1):
-        if i+j<=row+1:
-            res2+="*"+" "
-        else:
-            res2+=" "+" "
-    print(res2)
+# row=5 
+# for i in range(1,row+1):
+#     res2=""
+#     for j in range(1,row+1):
+#         if i+j<=row+1:
+#             res2+="*"+" "
+#         else:
+#             res2+=" "+" "
+#     print(res2)
+
+
             
 # row=5
 # for i in range (1,row+1):
