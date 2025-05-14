4b636eab125ac03284d14fcc8c6aa288a2601355
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-14 15:39:39 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1127,14 +1127,24 @@ print(calendar.month(2003,6))'''
 # * * * * *
 
 
-rows=5
+# rows=5
 
-for row in range(1,rows+1):
+# for row in range(1,rows+1):
     
-    for col in range(1,rows+1):
-        if row+col>=rows+1:
-            print("+",end=" ") 
+#     for col in range(1,rows+1):
+#         if row+col>=rows+1:
+#             print("+",end=" ") 
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+
+import math
+n=5
+for row in range(1,n+1):
+    for col in range(1,n+1):
+        if row==1 or row==5 or col==1 or col==5 or  (row==math.ceil(n/2) and col==math.ceil(n/2)):
+             print("*",end=" ")
         else:
             print(" ",end=" ")
     print()
-
