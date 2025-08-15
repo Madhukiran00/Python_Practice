f45792df2ebeeaf8e40093276902a1f693b754de
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-08-15 12:19:00 +0530

    cpp

--- CPP.cpp
+++ CPP.cpp
@@ -1,26 +1,8 @@
-#include<iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<< "Hello";
+// }
 
 
-
-
-class poly{
-public:
-    void fun(int x){
-        cout<< "Called fun() int:" << x << endl;
-    }
-
-
-void fun(double y){
-    cout<< "Called fun() with float:" << y << endl;
-}
-
-};
-
-int main(){
-  poly obj;
-  obj.fun(5);
-  obj.fun(4.56);
-  return 0;
-
-}
