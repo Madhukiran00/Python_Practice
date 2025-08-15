3fad6a7d9b2e6be2309f1e36cdc4f669461f6eb1
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-08-15 12:37:36 +0530

    cpp

--- CPP.cpp
+++ CPP.cpp
@@ -6,3 +6,55 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a=12;
+//     cout<<a;
+// }
+
+
+// int main(){
+//     cout<<"dgfgf" << endl;
+//     cout<<"sdgfddf";
+// }
+
+
+
+/*int main(){
+    cout<<"dgfdh\n\n";
+    cout<<"dhgfdhf";
+}*/
+
+// int main(){
+//     int n=12;
+//     int *k = &n;
+//     n=10;
+//     cout<< n<<k;
+// }
+
+
+// int main(){
+//     char n='r';
+//     int a=5;
+//     string st="madhu";
+//     bool d=true;
+
+//     cout<< n<<a <<st<<d;
+// }
+
+// int main(){
+//     int  a=45,b=56,c=67;
+//     cout<<a<<b<<c;
+
+// }
+
+
+// int main(){
+//     const int a=10;
+//     // a=8;
+//     cout<< a;
+// }
+
+
